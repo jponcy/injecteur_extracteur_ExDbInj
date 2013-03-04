17add69bd02920315d904f772c918d0e70a85bb9
Sylvain Thomas <thomsyl88@gmail.com>
2013-03-04 14:06:07 +0100

    #3 ExtractorMysql

---
+++ src/org/i2l/exdbinj/ExtractorMySql.java
@@ -0,0 +1,136 @@
+package org.i2l.exdbinj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ExtractorMySql {
+
+	private String url = null;
+	private String dbName = null;
+	private String userName = null;
+	private String password = null;
+	private Connection connect = null;
+	private Statement statement = null;
+	private ResultSet resultSet = null;
+
+	public ExtractorMySql(String e_url, String e_db, String e_user, String e_passwd){
+		url = "jdbc:mysql://"+ e_url +"/";
+		dbName = e_db;
+		userName = e_user;
+		password = e_passwd;
+		System.out.println("init mysql");
+	}
+	
+	public String getResult(String request){
+		try {
+			readDataBase(request);
+			return writeResultSet();
+		}
+		catch (Exception e) { e.printStackTrace(); System.out.println("toto"); }
+		return null;
+	}
+	
+	
+	private String writeResultSet() throws Exception {
+		String tmp = null;
+		while (resultSet.next()) {
+			
+			int nbColumn = resultSet.getMetaData().getColumnCount();
+			
+			for(int i=0 ; i < nbColumn ; i++){
+				//tmp += resultSet.getString(i);
+				tmp += resultSet.getString(i);
+				tmp += " : ";
+			}
+			
+			tmp += " / ";
+			/*
+			String id = resultSet.getString("idgroupe");
+			String name = resultSet.getString("nomgroupe");
+			String comment = resultSet.getString("commentairegroupe");
+			tmp += "id : " + id + " / group name : " + name + " / commentaire : " + comment;
+			System.out.println("id : " + id + " / group name : " + name + " / commentaire : " + comment);
+			*/
+		}
+		return tmp;
+	}
+	
+	
+
+	private void readDataBase(String request) throws Exception {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.getConnection(url + dbName, userName, password);
+			System.out.println("getConnect");
+			
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery(request);
+			System.out.println("good result");
+		}
+		catch (Exception e) { throw e; }
+		//finally { close(); }
+	}
+	
+	
+	/*
+	ResultSet rs = statement.executeQuery(req);
+
+	if (rs != null) {
+
+		// root elements
+		Element rootElement = doc.createElement(dbName);
+		doc.appendChild(rootElement);
+
+		while (rs.next()) {
+
+			Element table = doc.createElement("Etudiant");
+			rootElement.appendChild(table);
+
+			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+				Element node = doc.createElement(rs.getMetaData().getColumnName(i));
+				node.appendChild(doc.createTextNode(rs.getString(i)));
+				table.appendChild(node);
+			}
+
+		}
+
+		transformer.transform(source, result);
+
+		System.out.println("File saved!");
+
+	}
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	private void close() {
+		try {
+			if (resultSet != null) { resultSet.close(); }
+			if (statement != null) { statement.close(); }
+			if (connect != null) { connect.close(); }
+		}
+		catch (Exception e) { }
+	}
+}
